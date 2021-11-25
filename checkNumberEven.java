86e7c707378d5664c117b393ea70179f72ebbc19
islamG <islamgunebakan@gmail.com>
2021-11-25 16:11:01 +0100

    1v

---
+++ checkNumberEven.java
@@ -0,0 +1,33 @@
+package Projects_01;
+
+public class checkNumberEven {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+	}
+}
+
+    /*
+        Given an int check the number is even or not
+
+        if the number is even print true
+
+        if the number is odd print false
+
+        result should be true or false.
+
+     */
+
+
+     /*
+        Veri tipi int olan number verildiğinde, number  çift mi tek mi kontrol et
+
+        Eğer number çift ise  print true
+
+        Eğer number tek ise  print false
+
+        result true ya da false olmalı.
+
+     */
+
+
