324267c8e30f8b7318fa1a296c7b7935c1d2f9e8
Islam Günebakan <79874443+Gunebakan27@users.noreply.github.com>
2021-11-29 15:57:05 +0100

    Add files via upload

--- Arrays01.java
+++ Arrays01.java
@@ -1,38 +1,44 @@
-package day14statickeywordarrays;
+package day15arrays;
 
 import java.util.Arrays;
 
 public class Arrays01 {
 
 	public static void main(String[] args) {
-//How to create an Array
-		int arr1[]=new int[5];//int[] arr1=new int[5];
-							  //int []arr1=new int[5];
+
+	//create a String Array and print the elements in alphabetical order on the console in different lines
 		
-//How to print an Array on the console
-		System.out.println(Arrays.toString(arr1));//[0, 0, 0, 0, 0]
-//How to add elements into an Array
-		arr1[1]=11;
-		System.out.println(Arrays.toString(arr1));//[0, 11, 0, 0, 0]
+//		String sArr[]=new String[5];
+//		System.out.println(Arrays.toString(sArr));
+//		sArr[0]="Ali";
+//		sArr[1]="Bekir";
+//		sArr[2]="Zehra";
+//		sArr[3]="Yasin";
+//		sArr[4]="Mary";
 		
-		arr1[4]=25;
-		System.out.println(Arrays.toString(arr1));//[0, 11, 0, 0, 25]
+		String arr[]= {"Ali","Veli","Deli"};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
 		
-		arr1[0]=23;
-		arr1[2]=32;
-		arr1[3]=12;
-		System.out.println(Arrays.toString(arr1));
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
 		
-//How to read specific element from an Array
-		System.out.println(arr1[2]);//32
-		
-		//System.out.println(arr1[5]);//No red underline so no CTE
-									//ArrayIndexOutOfBoundsException: Run Time Error
-		//System.out.println(arr1.length);
-		System.out.println(arr1[arr1.length-1]);
-			
-		
-		
+//		String sArr[]= {"Ali", "Yasin", "Bekir", "Mary", "Zehra"};
+//		System.out.println(Arrays.toString(sArr));
+////How to sort Array elements
+//		Arrays.sort(sArr);
+//		System.out.println(Arrays.toString(sArr));
+//		
+//		for(int i=0;i<sArr.length;i++) {
+//			System.out.println(sArr[i]);
+//		}
+//		for(int i=sArr.length-1;i>=0;i--) {
+//			System.out.println(sArr[i]);
+//		}
+
+
+
 		
 	}
 
